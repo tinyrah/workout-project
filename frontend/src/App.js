1d59b08e3fc9549fca3eb8770b49c060ee26b6e5
Raul Hernandez <raulhernandezwork@gmail.com>
2023-12-22 21:32:50 -0600

    Preparing App.js for different pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
+import Overview from './components/Overview/Overview';
+import Panel from './components/Panel/Panel';
+import Navbar from './components/Navbar/Navbar';
 
 const App = () => {
   return (
@@ -13,6 +16,8 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Login onLogin={() => { /* handle login */ }} />} />
           <Route path="/register" element={<Register onRegister={() => { /* handle registration */ }} />} />
+          <Route path="/overview" element={<Overview />} />
+          <Route path="/panel" element={<Panel />} />
         </Routes>
       </div>
     </Router>
