56871e8855629ff4f3896ed30a1f8ce6ca31c206
Raul Hernandez <raulhernandezwork@gmail.com>
2023-12-22 21:13:39 -0600

    Routing for Register Page works

--- frontend/src/components/Register/Register.js
+++ frontend/src/components/Register/Register.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './Register.css';
 
-const Register = ({ onRegister }) => {
-  const handleSubmit = (event) => {
+const Register = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    // Implement registration logic here
-    onRegister();
+
+    // Simple front-end validation for example purposes
+    if (password !== confirmPassword) {
+      alert('Passwords do not match!');
+      return;
+    }
+
+    try {
+      // Make a POST request to your API endpoint
+      const response = await axios.post('http://localhost:4000/register', {
+        email,
+        password
+      });
+
+      // Handle the response from the server
+      if (response.status === 201) {
+        alert('Registration successful!');
+        // Redirect the user to login page or home page after registration
+        // window.location = '/login'; // Un-comment this line to enable redirection
+      }
+    } catch (error) {
+      if (error.response && error.response.status === 409) {
+        alert('User already exists!');
+      } else {
+        alert('Registration failed!');
+        console.error('Registration Error:', error);
+      }
+    }
   };
 
   return (
@@ -13,20 +44,34 @@ const Register = ({ onRegister }) => {
       <h2>Register</h2>
       <form onSubmit={handleSubmit}>
         <div className="input-group">
-          <label htmlFor="username">Username</label>
-          <input type="text" id="username" required />
-        </div>
-        <div className="input-group">
           <label htmlFor="email">Email</label>
-          <input type="email" id="email" required />
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
         </div>
         <div className="input-group">
           <label htmlFor="password">Password</label>
-          <input type="password" id="password" required />
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
         </div>
         <div className="input-group">
           <label htmlFor="confirm-password">Confirm Password</label>
-          <input type="password" id="confirm-password" required />
+          <input
+            type="password"
+            id="confirm-password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            required
+          />
         </div>
         <button type="submit">Register</button>
       </form>
